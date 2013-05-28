5503017f395b3c80aae92c7355ea86d5f8995411
aritra mukherjee <aritram1@gmail.com>
2013-05-28 22:39:36 +0530

    updated day - 1
    Signed-off-by: aritra mukherjee <aritram1@gmail.com>

---
+++ ContactManager/src/com/contact/contactmanager/ContactsList.java
@@ -0,0 +1,12 @@
+package com.contact.contactmanager;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+public class ContactsList /*extends ArrayAdapter<String>*/ {
+	
+	
+
+}
--- ContactManager/src/com/contact/contactmanager/MainActivity.java
+++ ContactManager/src/com/contact/contactmanager/MainActivity.java
@@ -1,15 +1,38 @@
 package com.contact.contactmanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TabWidget;
 
 public class MainActivity extends Activity {
-
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_page);
+        
+        ListView list = (ListView)findViewById(R.layout.splash_page);
+        
+        ArrayList<String> accountNames = new ArrayList<String>();
+        for(Account a : AccountManager.get(this).getAccounts()){
+        	accountNames.add(a.name);
+        }
+        if(accountNames.size() > 0){
+        	myadapter m = new myadapter(this, R.layout.account_entry, accountNames);
+        	list.setAdapter(m);
+        }
+        
+        
+        
     }
 
 
@@ -20,4 +43,15 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    private class myadapter extends ArrayAdapter<String>{
+
+		public myadapter(Context context, int textViewResourceId, List<String> objects) {
+			super(context, textViewResourceId, objects);			
+		}
+		
+    	
+    }
+
+    
 }
+
